5fb0826477af535be369040eebe0ee41d7231340
AmitwalaH <algoworks818@gmail.com>
2026-01-13 14:38:11 +0530

    Add ABOUT.md for all tracks

--- src/routes/track.js
+++ src/routes/track.js
@@ -17,7 +17,7 @@ router.get("/", listTracks);
 router.post("/:trackSlug/join", protect, joinTrack);  // ADD THIS LINE
 
 // GET /api/tracks/:trackSlug/concepts/:conceptSlug
-router.get("/:trackSlug/concepts/:conceptSlug", getConceptDetail);
+// router.get("/:trackSlug/concepts/:conceptSlug", getConceptDetail);
 
 // list categories for a track
 router.get("/:trackSlug/exercises", listCategories);
